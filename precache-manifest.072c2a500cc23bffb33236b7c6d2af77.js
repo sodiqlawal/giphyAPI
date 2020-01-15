6d12160fdb7ed320d2358fa0173e01e7d18b480a
sodiqlawal <sodiqlawal95@gmail.com>
2020-01-15 15:27:01 +0100

    Updates

---
+++ precache-manifest.072c2a500cc23bffb33236b7c6d2af77.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d36127a0512e3105ba2da17b0784a77",
+    "url": "/giphyAPI/index.html"
+  },
+  {
+    "revision": "1d3a67455d6577ae82ee",
+    "url": "/giphyAPI/static/css/2.f8399ec5.chunk.css"
+  },
+  {
+    "revision": "b7869097c0c902af5475",
+    "url": "/giphyAPI/static/css/main.4c610b38.chunk.css"
+  },
+  {
+    "revision": "1d3a67455d6577ae82ee",
+    "url": "/giphyAPI/static/js/2.6bee45ce.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/giphyAPI/static/js/2.6bee45ce.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b7869097c0c902af5475",
+    "url": "/giphyAPI/static/js/main.f2cc897d.chunk.js"
+  },
+  {
+    "revision": "ac3771ece3ac613316a7",
+    "url": "/giphyAPI/static/js/runtime-main.c6c51321.js"
+  }
+]);
